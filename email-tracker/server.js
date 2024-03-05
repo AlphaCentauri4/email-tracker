0d19d6b9723cfca4c3013f4239cff2fbb0420511
AlphaCentauri4 <94246993+AlphaCentauri4@users.noreply.github.com>
2024-03-04 21:48:58 -0500

    Update server.js

--- email-tracker/server.js
+++ email-tracker/server.js
@@ -1,9 +1,18 @@
 // server.js
 const express = require('express');
+const mongoose = require('mongoose');
 const trackerRoutes = require('./routes/tracker');
 
 const app = express();
 const PORT = process.env.PORT || 10000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/emailTracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Failed to connect to MongoDB', err));
 
 // Middleware
 app.use(express.json());
